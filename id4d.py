6a3f31761756993826e9cd865971f88d84b1056b
Ted Dunstone <ted@biometix.com>
2021-04-29 16:55:15 +1000

    Add files via upload

--- id4d.py
+++ id4d.py
@@ -19,32 +19,35 @@ standards = pd.read_csv('standards.csv')
 #df = pd.DataFrame({'a':[1,2,3,4],'b':[1,2,3,4]})
 #st.write(modalities)
 # ![Image Description](https://id4d.worldbank.org/themes/id4d/logo.png)
-if modalities and apptype:
-    st.write(f'''
-        
-        # Standards Requirements
+checked={}
 
-        The following are base level requirements that are recommended for a 
-        foundational ID having attributes  
-        *{', '.join(modalities)}*
-        ''')
-    last_cat = ""
-#    modalities.extend(['All'])
-    standards=standards[standards['Modality'].isin(modalities+['All'])]
-    standards=standards.sort_values('Category')
-    checked={}
-    for row in standards.itertuples():
-        if type(row.Standard)==type(''):
-            if row.Category!=last_cat:
-                st.header(row.Category)
-            cols = st.beta_columns(4)
-            checked[row.Standard]=cols[0].checkbox(row.Standard)
-            cols[1].write('**'+row.Standard+'**')
-            cols[2].write(row.Description)
-            if row.Modality!='All':
-                cols[2].write('Attribute :'+row.Modality)
-            if show_link:
-                cols[3].write(f"[Link]({row.Link})")
-            last_cat = row.Category
-    st.write(checked)
+if modalities and apptype:
+    with st.beta_expander('Settings',True):
+        st.write(f'''
+            
+            # Standards Requirements
+
+            The following are base level requirements that are recommended for a 
+            foundational ID having attributes  
+            *{', '.join(modalities)}*
+            ''')
+        last_cat = ""
+    #    modalities.extend(['All'])
+        standards=standards[standards['Modality'].isin(modalities+['All'])]
+        standards=standards.sort_values('Category')
+        for row in standards.itertuples():
+            if type(row.Standard)==type(''):
+                if row.Category!=last_cat:
+                    st.header(row.Category)
+                cols = st.beta_columns(4)
+                checked[row.Standard]=cols[0].checkbox(row.Standard)
+                cols[1].write('**'+row.Standard+'**')
+                cols[2].write(row.Description)
+                if row.Modality!='All':
+                    cols[2].write('Attribute :'+row.Modality)
+                if show_link:
+                    cols[3].write(f"[Link]({row.Link})")
+                last_cat = row.Category
+    with st.beta_expander('final'):
+        st.write(checked)
     
