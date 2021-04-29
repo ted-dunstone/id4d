f096c9dc26f6cf093fd4d964e5a1e94a23638268
Ted Dunstone <ted@biometix.com>
2021-04-29 16:39:37 +1000

    Add files via upload

--- id4d.py
+++ id4d.py
@@ -8,14 +8,16 @@ st.set_page_config(
 st.sidebar.header('ID4D')
 st.sidebar.write('The following app will help to select standards should be utilized as part of a foundational identity system.')
 st.sidebar.write('The answers provided below will customise the standards list.')
-modalities=st.sidebar.select_slider("Population Size",['1-5M','5-50M','50-100M','100M+'])
-modalities=st.sidebar.multiselect("Attributes",['Face','Fingerprint','Iris','Mobile'])
+#modalities=st.sidebar.select_slider("Population Size",['1-5M','5-50M','50-100M','100M+'])
 apptype=st.sidebar.multiselect("Applications Required",['Foundational ID','Population Registry'])
+modalities=st.sidebar.multiselect("Attributes",['Face','Fingerprint','Iris'])
 
+if st.sidebar.checkbox('Require mobile applications',False):
+    modalities+=['Mobile'] 
+show_link = st.sidebar.checkbox('Show link to Standard')
 standards = pd.read_csv('standards.csv')
 #df = pd.DataFrame({'a':[1,2,3,4],'b':[1,2,3,4]})
 #st.write(modalities)
-standards.sort_values('Category')
 # ![Image Description](https://id4d.worldbank.org/themes/id4d/logo.png)
 if modalities and apptype:
     st.write(f'''
@@ -29,16 +31,20 @@ if modalities and apptype:
     last_cat = ""
 #    modalities.extend(['All'])
     standards=standards[standards['Modality'].isin(modalities+['All'])]
-    
+    standards=standards.sort_values('Category')
+    checked={}
     for row in standards.itertuples():
         if type(row.Standard)==type(''):
             if row.Category!=last_cat:
                 st.header(row.Category)
-            cols = st.beta_columns(3)
-            cols[0].write('**'+row.Standard+'**')
-            cols[1].write(row.Description)
+            cols = st.beta_columns(4)
+            checked[row.Standard]=cols[0].checkbox(row.Standard)
+            cols[1].write('**'+row.Standard+'**')
+            cols[2].write(row.Description)
             if row.Modality!='All':
-                cols[1].write('Attribute :'+row.Modality)
-            #cols[2].write(row.Category)
+                cols[2].write('Attribute :'+row.Modality)
+            if show_link:
+                cols[3].write(f"[Link]({row.Link})")
             last_cat = row.Category
+    st.write(checked)
     
